79b9f7b500ea4f9cca2ba9217ad0b2bbd9f3883e
52Blue05 <trinhnam30092005@gmail.com>
2026-03-27 13:23:50 +0700

    Add initial implementation of helloworld class with greeting messages

---
+++ Lab01/helloworld.java
@@ -0,0 +1,8 @@
+package Lab01;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Xin chào các bạn!");
+        System.err.println("Hello world!");
+    }
+}
