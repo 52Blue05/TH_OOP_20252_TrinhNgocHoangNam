ee4eadb79780cc7f2b1157e3102cb005d2a7a9a4
52Blue05 <trinhnam30092005@gmail.com>
2026-03-27 13:44:17 +0700

    Add FirstDialog and HelloNameDialog classes with greeting functionality

---
+++ Lab01/FirstDialog.java
@@ -0,0 +1,9 @@
+package Lab01;
+
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Xin chào các bạn!");
+    }
+}
